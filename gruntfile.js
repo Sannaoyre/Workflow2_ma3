df271102525ac0fa00b11abe2c52244dec82857d
Sanna Oyre <sanna-f@hotmail.com>
2019-02-12 16:32:06 +0100

    typo

--- gruntfile.js
+++ gruntfile.js
@@ -52,6 +52,7 @@ module.exports = function (grunt) {
 
 	});
 
+
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
